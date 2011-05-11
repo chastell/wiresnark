0281f84dfa56648c20823fd5b9f220578cabe304
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-05-11 10:02:09 +0200

    eth_{src,dst} → eth_{s,d}addr

--- iface_test.rb
+++ iface_test.rb
@@ -3,36 +3,36 @@ require 'packetfu'
 require 'trollop'
 
 opts = Trollop.options do
-  opt :count,    'packet count',          :default => 10
-  opt :diface,   'destination interface', :default => 'lo'
-  opt :siface,   'source interface',      :default => 'lo'
-  opt :eth_dst,  'destination MAC',       :default => 'random'
-  opt :eth_src,  'source MAC',            :default => 'random'
-  opt :ip_daddr, 'destination IP',        :default => 'random'
-  opt :ip_saddr, 'source IP',             :default => 'random'
-  opt :payload,  'payload',               :default => 'random'
-  opt :sleep,    'seconds to sleep',      :default => 0
-  opt :type,     'packet type',           :default => 'TCP'
+  opt :count,     'packet count',          :default => 10
+  opt :diface,    'destination interface', :default => 'lo'
+  opt :siface,    'source interface',      :default => 'lo'
+  opt :eth_daddr, 'destination MAC',       :default => 'random'
+  opt :eth_saddr, 'source MAC',            :default => 'random'
+  opt :ip_daddr,  'destination IP',        :default => 'random'
+  opt :ip_saddr,  'source IP',             :default => 'random'
+  opt :payload,   'payload',               :default => 'random'
+  opt :sleep,     'seconds to sleep',      :default => 0
+  opt :type,      'packet type',           :default => 'TCP'
 end
 
 types = ['ARP', 'Eth', 'ICMP', 'IP', 'IPv6', 'TCP', 'UDP']
 Trollop.die :type, "must be one of #{types.join ', '}" unless types.include? opts[:type]
 
-opts[:eth_dst]  = Array.new(6) { rand(256).to_s(16).rjust(2, '0') }.join(':') if opts[:eth_dst]  == 'random'
-opts[:eth_src]  = Array.new(6) { rand(256).to_s(16).rjust(2, '0') }.join(':') if opts[:eth_src]  == 'random'
-opts[:ip_daddr] = Array.new(4) { rand(256) }.join('.')                        if opts[:ip_daddr] == 'random'
-opts[:ip_saddr] = Array.new(4) { rand(256) }.join('.')                        if opts[:ip_saddr] == 'random'
-opts[:payload]  = rand.to_s                                                   if opts[:payload]  == 'random'
+opts[:eth_daddr] = Array.new(6) { rand(256).to_s(16).rjust(2, '0') }.join(':') if opts[:eth_daddr] == 'random'
+opts[:eth_saddr] = Array.new(6) { rand(256).to_s(16).rjust(2, '0') }.join(':') if opts[:eth_saddr] == 'random'
+opts[:ip_daddr]  = Array.new(4) { rand(256) }.join('.')                        if opts[:ip_daddr]  == 'random'
+opts[:ip_saddr]  = Array.new(4) { rand(256) }.join('.')                        if opts[:ip_saddr]  == 'random'
+opts[:payload]   = rand.to_s                                                   if opts[:payload]   == 'random'
 
 puts "generating #{opts[:count]} #{opts[:type]} packets"
 packets = Array.new opts[:count] do
   packet = eval "PacketFu::#{opts[:type]}Packet.new"
   begin
-    packet.eth_dst  = opts[:eth_dst]
-    packet.eth_src  = opts[:eth_src]
-    packet.ip_daddr = opts[:ip_daddr]
-    packet.ip_saddr = opts[:ip_saddr]
-    packet.payload  = opts[:payload]
+    packet.eth_daddr = opts[:eth_daddr]
+    packet.eth_saddr = opts[:eth_saddr]
+    packet.ip_daddr  = opts[:ip_daddr]
+    packet.ip_saddr  = opts[:ip_saddr]
+    packet.payload   = opts[:payload]
   rescue NoMethodError
   end
   packet
