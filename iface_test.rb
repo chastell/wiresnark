4c93bd17dbcbf7c528504a96d7776f28cdf07a9a
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-05-11 08:18:56 +0200

    give the test script some much-needed Trollop love

---
+++ iface_test.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'packetfu'
+require 'trollop'
+
+opts = Trollop.options do
+  opt :count,   'packet count',          :default => 10
+  opt :d_iface, 'destination interface', :default => 'lo'
+  opt :s_iface, 'source interface',      :default => 'lo'
+  opt :type,    'packet type',           :default => 'TCP'
+end
+
+puts "generating #{opts[:count]} #{opts[:type]} packets…"
+packets = (1..opts[:count]).map do
+  packet = PacketFu::TCPPacket.new
+  packet.ip_saddr = [rand(256), rand(256), rand(256), rand(256)].join '.'
+  packet.ip_daddr = [rand(256), rand(256), rand(256), rand(256)].join '.'
+  packet.payload  = rand.to_s
+  packet.to_s
+end
+
+puts "starting capture on #{opts[:d_iface]}…"
+capture = PacketFu::Capture.new :iface => opts[:d_iface]
+capture.start
+
+puts "injecting packets into #{opts[:s_iface]}…"
+inject = PacketFu::Inject.new :iface => opts[:s_iface]
+inject.array_to_wire :array => packets
+
+capture.save
+
+puts "captured #{capture.array.size} packets…"
+print '*NOT* ' unless (packets - capture.array).empty?
+puts 'all generated captured!'
