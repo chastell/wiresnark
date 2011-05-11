b469149e67ad8546d934c45982363bf909a458ee
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-05-11 09:04:06 +0200

    introducing --ip-daddr and --ip-saddr

--- iface_test.rb
+++ iface_test.rb
@@ -3,17 +3,22 @@ require 'packetfu'
 require 'trollop'
 
 opts = Trollop.options do
-  opt :count,   'packet count',          :default => 10
-  opt :d_iface, 'destination interface', :default => 'lo'
-  opt :s_iface, 'source interface',      :default => 'lo'
-  opt :type,    'packet type',           :default => 'TCP'
+  opt :count,    'packet count',          :default => 10
+  opt :d_iface,  'destination interface', :default => 'lo'
+  opt :s_iface,  'source interface',      :default => 'lo'
+  opt :ip_daddr, 'destination IP',        :default => 'random'
+  opt :ip_saddr, 'source IP',             :default => 'random'
+  opt :type,     'packet type',           :default => 'TCP'
 end
+
+opts[:ip_daddr] = Array.new(4) { rand(256) }.join('.') if opts[:ip_daddr] == 'random'
+opts[:ip_saddr] = Array.new(4) { rand(256) }.join('.') if opts[:ip_saddr] == 'random'
 
 puts "generating #{opts[:count]} #{opts[:type]} packets"
 packets = Array.new opts[:count] do
   packet = PacketFu::TCPPacket.new
-  packet.ip_saddr = [rand(256), rand(256), rand(256), rand(256)].join '.'
-  packet.ip_daddr = [rand(256), rand(256), rand(256), rand(256)].join '.'
+  packet.ip_daddr = opts[:ip_daddr]
+  packet.ip_saddr = opts[:ip_saddr]
   packet.payload  = rand.to_s
   packet
 end
