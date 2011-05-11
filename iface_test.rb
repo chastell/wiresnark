3cbb57afbf989a73ed637951847282c02c0ed014
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-05-11 09:11:23 +0200

    introducing --payload

--- iface_test.rb
+++ iface_test.rb
@@ -8,18 +8,20 @@ opts = Trollop.options do
   opt :siface,   'source interface',      :default => 'lo'
   opt :ip_daddr, 'destination IP',        :default => 'random'
   opt :ip_saddr, 'source IP',             :default => 'random'
+  opt :payload,  'payload',               :default => 'random'
   opt :type,     'packet type',           :default => 'TCP'
 end
 
 opts[:ip_daddr] = Array.new(4) { rand(256) }.join('.') if opts[:ip_daddr] == 'random'
 opts[:ip_saddr] = Array.new(4) { rand(256) }.join('.') if opts[:ip_saddr] == 'random'
+opts[:payload]  = rand.to_s                            if opts[:payload]  == 'random'
 
 puts "generating #{opts[:count]} #{opts[:type]} packets"
 packets = Array.new opts[:count] do
   packet = PacketFu::TCPPacket.new
   packet.ip_daddr = opts[:ip_daddr]
   packet.ip_saddr = opts[:ip_saddr]
-  packet.payload  = rand.to_s
+  packet.payload  = opts[:payload]
   packet
 end
 
