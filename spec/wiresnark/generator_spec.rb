e2782e71abd7746ca2cc2fa27242efbc8ab71909
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-06-27 01:45:51 +0200

    unmock Generator spec

--- spec/wiresnark/generator_spec.rb
+++ spec/wiresnark/generator_spec.rb
@@ -3,26 +3,20 @@ module Wiresnark module Generator
   describe '.generate' do
 
     it 'returns an Array of Packets (one Eth packet by default)' do
-      Packet.should_receive(:new).once.with(type: 'Eth').and_return packet = mock
-      Generator.generate {}.should == [packet]
+      Generator.generate {}.should == [Packet.new(type: 'Eth')]
     end
 
     it 'makes it possible to set the count in the passed block' do
-      Packet.should_receive(:new).exactly(3).times.and_return p1 = mock, p2 = mock, p3 = mock
-      Generator.generate { count 3 }.should == [p1, p2, p3]
+      Generator.generate { count 3 }.should == [Packet.new(type: 'Eth'), Packet.new(type: 'Eth'), Packet.new(type: 'Eth')]
     end
 
     it 'makes it possible to set the type in the passed block' do
-      Packet.should_receive(:new).with(type: 'TCP').and_return packet = mock
-      Generator.generate { type 'TCP' }.should == [packet]
+      Generator.generate { type 'TCP' }.should == [Packet.new(type: 'TCP')]
     end
 
     it 'resets on every generation' do
-      Packet.should_receive(:new).twice.with type: 'IPv6'
-      Generator.generate { count 2; type 'IPv6' }
-
-      Packet.should_receive(:new).once.with type: 'Eth'
-      Generator.generate {}
+      Generator.generate { count 2; type 'IPv6' }.should == [Packet.new(type: 'IPv6'), Packet.new(type: 'IPv6')]
+      Generator.generate {}.should == [Packet.new(type: 'Eth')]
     end
 
   end
