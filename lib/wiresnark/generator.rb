c0e0cde9ce808f35e98d46b722f9f9e0cf842eea
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-06-27 01:24:37 +0200

    introducing Generator.type

--- lib/wiresnark/generator.rb
+++ lib/wiresnark/generator.rb
@@ -5,11 +5,15 @@ module Wiresnark module Generator
   def generate &packet_spec
     gen = Object.new.extend self
     gen.instance_eval &packet_spec
-    Array.new(gen.count) { Packet.new(type: 'Eth') }
+    Array.new(gen.count) { Packet.new type: gen.type }
   end
 
   def count count = nil
     count ? @count = count : @count ||= 1
+  end
+
+  def type type = nil
+    type ? @type = type : @type ||= 'Eth'
   end
 
 end end
--- spec/wiresnark/generator_spec.rb
+++ spec/wiresnark/generator_spec.rb
@@ -12,10 +12,16 @@ module Wiresnark module Generator
       Generator.generate { count 3 }.should == [p1, p2, p3]
     end
 
-    it 'resets the count on every generation' do
-      Packet.should_receive(:new).twice
-      Generator.generate { count 2 }
-      Packet.should_receive(:new).once
+    it 'makes it possible to set the type in the passed block' do
+      Packet.should_receive(:new).with(type: 'TCP').and_return packet = mock
+      Generator.generate { type 'TCP' }.should == [packet]
+    end
+
+    it 'resets on every generation' do
+      Packet.should_receive(:new).twice.with type: 'IPv6'
+      Generator.generate { count 2; type 'IPv6' }
+
+      Packet.should_receive(:new).once.with type: 'Eth'
       Generator.generate {}
     end
 
