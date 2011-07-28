849dc733f33a5ab5f78f9062d64662ac7db6085b
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-07-28 03:30:33 +0200

    simplify Generator::DSL’s spec

--- spec/wiresnark/generator/dsl_spec.rb
+++ spec/wiresnark/generator/dsl_spec.rb
@@ -4,55 +4,10 @@ module Wiresnark describe Generator::DSL do
     @env = Object.new.extend Generator::DSL
   end
 
-  describe '#count' do
-
-    it 'allows setting count, defaulting to 1' do
-      @env.params[:count].should == 1
-      @env.count 7
-      @env.params[:count].should == 7
-    end
-
-  end
-
-  describe '#destination_ip' do
-
-    it 'allows setting destination IP address' do
-      @env.destination_ip '1.2.3.4'
-      @env.params[:destination_ip].should == '1.2.3.4'
-    end
-
-  end
-
-  describe '#destination_ipv6' do
-
-    it 'allows setting destination IPv6 address' do
-      @env.destination_ipv6 '1111:2222:3333:4444:5555:6666:7777:8888'
-      @env.params[:destination_ipv6].should == '1111:2222:3333:4444:5555:6666:7777:8888'
-    end
-
-  end
-
-  describe '#destination_mac' do
-
-    it 'allows setting destination MAC address' do
-      @env.destination_mac '11:22:33:44:55:66'
-      @env.params[:destination_mac].should == '11:22:33:44:55:66'
-    end
-
-  end
-
-  describe '#iip_byte' do
-
-    it 'allows setting IIP byte' do
-      @env.iip_byte 2
-      @env.params[:iip_byte].should == 2
-    end
-
-  end
-
   describe '#params' do
 
-    it 'allows reading the current state of params' do
+    it 'allows reading the current state of params, defaulting count to 1' do
+      @env.params[:count].should == 1
       @env.count 7
       @env.payload 'LOAD "*",8,1'
       @env.params[:count].should   == 7
@@ -61,45 +16,36 @@ module Wiresnark describe Generator::DSL do
 
   end
 
-  describe '#payload' do
+  describe '#{params}' do
 
-    it 'allows setting payload' do
+    it 'allows setting the relevant params' do
+      @env.count 7
+      @env.params[:count].should == 7
+
+      @env.destination_ip '1.2.3.4'
+      @env.params[:destination_ip].should == '1.2.3.4'
+
+      @env.destination_ipv6 '1111:2222:3333:4444:5555:6666:7777:8888'
+      @env.params[:destination_ipv6].should == '1111:2222:3333:4444:5555:6666:7777:8888'
+
+      @env.destination_mac '11:22:33:44:55:66'
+      @env.params[:destination_mac].should == '11:22:33:44:55:66'
+
+      @env.iip_byte 2
+      @env.params[:iip_byte].should == 2
+
       @env.payload 'LOAD "*",8,1'
       @env.params[:payload].should == 'LOAD "*",8,1'
-    end
 
-  end
-
-  describe '#source_ip' do
-
-    it 'allows setting source IP address' do
       @env.source_ip '1.2.3.4'
       @env.params[:source_ip].should == '1.2.3.4'
-    end
 
-  end
-
-  describe '#source_ipv6' do
-
-    it 'allows setting source IPv6 address' do
       @env.source_ipv6 '1111:2222:3333:4444:5555:6666:7777:8888'
       @env.params[:source_ipv6].should == '1111:2222:3333:4444:5555:6666:7777:8888'
-    end
 
-  end
-
-  describe '#source_mac' do
-
-    it 'allows setting source MAC address' do
       @env.source_mac '11:22:33:44:55:66'
       @env.params[:source_mac].should == '11:22:33:44:55:66'
-    end
 
-  end
-
-  describe '#type' do
-
-    it 'allows setting type' do
       @env.type 'TCP'
       @env.params[:type].should == 'TCP'
     end
