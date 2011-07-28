69db57e20f22ed5743481eca45f5e3636722a032
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-07-28 03:32:00 +0200

    simplify Generator’s spec

--- spec/wiresnark/generator_spec.rb
+++ spec/wiresnark/generator_spec.rb
@@ -3,11 +3,11 @@ module Wiresnark module Generator
   describe '.generate' do
 
     it 'returns an Array of Packets (one Eth packet by default)' do
-      Generator.generate {}.should == [Packet.new(type: 'Eth')]
+      Generator.generate {}.should == [Packet.new]
     end
 
     it 'makes it possible to set the count in the passed block' do
-      Generator.generate { count 3 }.should == [Packet.new(type: 'Eth'), Packet.new(type: 'Eth'), Packet.new(type: 'Eth')]
+      Generator.generate { count 3 }.should == [Packet.new, Packet.new, Packet.new]
     end
 
     it 'makes it possible to set the type in the passed block' do
@@ -16,7 +16,7 @@ module Wiresnark module Generator
 
     it 'resets on every generation' do
       Generator.generate { count 2; type 'IPv6' }.should == [Packet.new(type: 'IPv6'), Packet.new(type: 'IPv6')]
-      Generator.generate {}.should == [Packet.new(type: 'Eth')]
+      Generator.generate {}.should == [Packet.new]
     end
 
   end
