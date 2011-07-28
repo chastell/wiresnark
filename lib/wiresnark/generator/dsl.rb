7b86bb64440c63a79198799a6c17ce3c8bcb7aed
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-07-28 02:16:50 +0200

    Generator::DSL: simplify internals

--- lib/wiresnark/generator/dsl.rb
+++ lib/wiresnark/generator/dsl.rb
@@ -2,28 +2,38 @@ module Wiresnark module Generator::DSL
 
   private
 
-  def self.dsl_attr name, default
+  def self.dsl_attr name
     define_method name do |value = nil|
-      ivar = :"@#{name}"
-      case
-      when value                       then instance_variable_set ivar, value
-      when instance_variable_get(ivar) then instance_variable_get ivar
-      else                                  instance_variable_set ivar, default
-      end
+      value.nil? ? params[name] : params[name] = value
     end
+  end
+
+  def params
+    @params ||= {
+      count:            1,
+      destination_ip:   '0.0.0.0',
+      destination_ipv6: '0000:0000:0000:0000:0000:0000:0000:0000',
+      destination_mac:  '00:00:00:00:00:00',
+      iip_byte:         1,
+      payload:          '',
+      source_ip:        '0.0.0.0',
+      source_ipv6:      '0000:0000:0000:0000:0000:0000:0000:0000',
+      source_mac:       '00:00:00:00:00:00',
+      type:             'Eth',
+    }
   end
 
   public
 
-  dsl_attr :count,            1
-  dsl_attr :destination_ip,   '0.0.0.0'
-  dsl_attr :destination_ipv6, '0000:0000:0000:0000:0000:0000:0000:0000'
-  dsl_attr :destination_mac,  '00:00:00:00:00:00'
-  dsl_attr :iip_byte,         1
-  dsl_attr :payload,          ''
-  dsl_attr :source_ip,        '0.0.0.0'
-  dsl_attr :source_ipv6,      '0000:0000:0000:0000:0000:0000:0000:0000'
-  dsl_attr :source_mac,       '00:00:00:00:00:00'
-  dsl_attr :type,             'Eth'
+  dsl_attr :count
+  dsl_attr :destination_ip
+  dsl_attr :destination_ipv6
+  dsl_attr :destination_mac
+  dsl_attr :iip_byte
+  dsl_attr :payload
+  dsl_attr :source_ip
+  dsl_attr :source_ipv6
+  dsl_attr :source_mac
+  dsl_attr :type
 
 end end
