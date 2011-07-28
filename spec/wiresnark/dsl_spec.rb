c360b007ef5fb226097f2644e8514ce36a7b162e
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-07-28 11:04:15 +0200

    æsthetic fix

--- spec/wiresnark/dsl_spec.rb
+++ spec/wiresnark/dsl_spec.rb
@@ -4,7 +4,7 @@ module Wiresnark describe DSL do
 
     it 'sends the desired packets to the given interface' do
       iface_name = 'DSL.send_packets_to spec interface'
-      Interfaces[iface_name].should_receive(:inject).with([Packet.new(type: 'TCP'), Packet.new(type: 'TCP')])
+      Interfaces[iface_name].should_receive(:inject).with [Packet.new(type: 'TCP'), Packet.new(type: 'TCP')]
 
       DSL.send_packets_to iface_name do
         count 2
