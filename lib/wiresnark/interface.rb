abadce2e1a94a7e1e456ac52025783cc99312e43
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-07-28 10:46:53 +0200

    make Interface#inject print what’s injected

--- lib/wiresnark/interface.rb
+++ lib/wiresnark/interface.rb
@@ -4,8 +4,10 @@ module Wiresnark class Interface
     @name = name
   end
 
-  def inject packets
+  def inject packets, output = StringIO.new
     PacketFu::Inject.new(@name).inject array: packets.map(&:to_bin)
+    output.puts "injected into #{@name}:"
+    packets.each { |packet| output.puts "\t#{packet}" }
   end
 
 end end
--- spec/wiresnark/interface_spec.rb
+++ spec/wiresnark/interface_spec.rb
@@ -12,6 +12,22 @@ module Wiresnark describe Interface do
       Interface.new(iface_name).inject [Packet.new(payload: 'foo'), Packet.new(payload: 'bar')]
     end
 
+    it 'puts the information about injected Packets to the passed IO' do
+      iface_name = 'Interface#inject spec interface'
+
+      inject = mock
+      PacketFu::Inject.should_receive(:new).with(iface_name).and_return inject
+      inject.should_receive :inject
+
+      Interface.new(iface_name).inject [Packet.new(payload: 'foo'), Packet.new(payload: 'bar')], output = StringIO.new
+      output.rewind
+      output.read.should == <<-END
+injected into #{iface_name}:
+\tEth  00 00 00 00 00 00 00 00 00 00 00 00 08 00 66 6f 6f
+\tEth  00 00 00 00 00 00 00 00 00 00 00 00 08 00 62 61 72
+      END
+    end
+
   end
 
 end end
