047a69eea3ab39d99f9d513ee53e1c0ee2c3b97f
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2011-08-01 01:13:37 +0200

    æsthetic fix

--- lib/wiresnark/generator.rb
+++ lib/wiresnark/generator.rb
@@ -1,8 +1,6 @@
 module Wiresnark module Generator
 
-  extend self
-
-  def generate &packet_spec
+  def self.generate &packet_spec
     gen = Object.new.extend Packet::DSL
     gen.instance_eval &packet_spec
     Array.new(gen.count) { Packet.new gen.params }
