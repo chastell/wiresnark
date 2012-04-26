62adfadcd34abaabe1e8526f616de9e478329bc6
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-04-26 13:01:28 +0200

    shadow used to walk with me (#1 works)

--- lib/wiresnark/iip/getter.rb
+++ lib/wiresnark/iip/getter.rb
@@ -6,10 +6,10 @@ module Wiresnark module IIP class Getter
   def get
     Nokogiri::XML::Builder.new do |xml|
       xml.interfaces do
-        @net_fpga.ports.each.with_index do |port, i|
-          xml.interface name: "eth#{i}" do
-            port.type_map.each.with_index do |(type, value), i|
-              xml.v_port name: "v_#{i+1}" do
+        @net_fpga.ports.each.with_index do |port, e|
+          xml.interface name: "eth#{e}" do
+            port.type_map.each.with_index do |(type, value), p|
+              xml.v_port name: "v_#{p+1}" do
                 xml.pi type: type do
                   xml.PIH value.to_s(2).rjust 3, '0'
                 end
