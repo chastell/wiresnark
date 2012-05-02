d8ef1d39d16ffe1067ff4189df7dbe0f34484247
Piotr Szotkowski <p.szotkowski@tele.pw.edu.pl>
2012-05-02 09:11:28 +0200

    hug them blocks (#1 works)

--- spec/wiresnark/dsl/wiresnark_dsl_spec.rb
+++ spec/wiresnark/dsl/wiresnark_dsl_spec.rb
@@ -6,8 +6,8 @@ module Wiresnark describe DSL::WiresnarkDSL do
       env = Object.new.extend DSL::WiresnarkDSL
       block_a = -> { interface 'iface_a'; count 7 }
       block_b = -> { interface 'iface_b'; count 3 }
-      env.generator &block_a
-      env.generator &block_b
+      env.generator(&block_a)
+      env.generator(&block_b)
       env.generator_blocks.must_equal [block_a, block_b]
     end
   end
@@ -17,8 +17,8 @@ module Wiresnark describe DSL::WiresnarkDSL do
       env = Object.new.extend DSL::WiresnarkDSL
       block_a = -> { interface 'iface_a' }
       block_b = -> { interface 'iface_b' }
-      env.monitor &block_a
-      env.monitor &block_b
+      env.monitor(&block_a)
+      env.monitor(&block_b)
       env.monitor_blocks.must_equal [block_a, block_b]
     end
   end
